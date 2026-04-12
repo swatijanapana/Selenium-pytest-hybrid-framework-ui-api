ac0c1772741af0bfe95e03dc8c380429faae5d16
swatij777 <swati.janapana@gmail.com>
2026-04-11 19:53:40 -0500

    Add pytest hook for automatic screenshot capture on test failure

--- Tests/conftest.py
+++ Tests/conftest.py
@@ -53,3 +53,13 @@ def driver(request):
     driver.quit()
 
 
+@pytest.hookimpl(hookwrapper=True)
+def pytest_runtest_makereport(item, call):
+        outcome = yield
+        rep = outcome.get_result()
+
+        if  rep.when == "call" and rep.failed:
+            driver = item.funcargs.get("driver") or getattr(item.instance,"driver",None)
+            if driver:
+             driver.save_screenshot(f"Reports/Screenshots/{item.name}.png")
+
